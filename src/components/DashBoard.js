39e201bcf87ee1a2ac809ab8d30e2224a9bfcf4d
Mostafa-ashraf19 <mostaphaashraf1996@gmail.com>
2021-05-12 22:55:11 +0200

    dashboard view

---
+++ src/components/DashBoard.js
@@ -0,0 +1,45 @@
+import React, {Component} from 'react'
+import {connect} from 'react-redux'
+import User from './User'
+import '../index.css'
+
+class Dashboard extends Component {
+
+    render() {
+        const {users} = this.props
+
+        return (
+            <div>
+                {
+                    <ul>
+                        {
+                            users.map((user) => 
+                            <li className='user-data' key={user.name}>
+                                <User user={user}/>
+                            </li>
+                            )
+                        }
+                    </ul>
+                }
+            </div>
+                    // <ul>
+                    //     {
+                            
+                    //     }
+                    // </ul> 
+        )
+    }
+
+}
+
+function mapStateToProps({users}){
+    return {
+        users: Object.keys(users).length <= 0 ? [] : Object.values(users).sort((a,b)=> {
+           return (Object.keys(b.answers).length + b.questions.length) - (Object.keys(a.answers).length + a.questions.length)
+        }),
+    }
+}
+
+const ConnectedDashBoard = connect(mapStateToProps)(Dashboard)
+
+export default ConnectedDashBoard
