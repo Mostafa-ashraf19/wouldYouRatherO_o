d8cf1c60b56c68dcbe1d7fd22c33f4a9f179e96e
Mostafa-ashraf19 <mostaphaashraf1996@gmail.com>
2021-05-14 15:21:47 +0200

    supports add question

--- src/reducers/questions.js
+++ src/reducers/questions.js
@@ -1,4 +1,4 @@
-import {GET_QUESTIONS} from '../actions/questions'
+import {GET_QUESTIONS,ADD_QUESTION} from '../actions/questions'
 
 
 export default function questions(state = {}, action){
@@ -9,6 +9,11 @@ export default function questions(state = {}, action){
                 ...state,
                 ...action.questions
             }
+        case ADD_QUESTION: 
+            return {
+                ...state,
+                [action.question.id] : action.question,
+            }    
         default:
             return state
     }
