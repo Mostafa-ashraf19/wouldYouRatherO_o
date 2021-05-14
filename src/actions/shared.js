f8bea7e5aa1bc2e4e20d26ef49ba848fc621818c
Mostafa-ashraf19 <mostaphaashraf1996@gmail.com>
2021-05-14 15:25:32 +0200

    shared actions

---
+++ src/actions/shared.js
@@ -0,0 +1,26 @@
+import {hideLoading, showLoading} from 'react-redux-loading-bar'
+
+import {handleLogin} from './authentication'
+import {handleGetQustions} from './questions'
+import {handleGetUsers} from './users'
+
+const CLEAR_STORE = 'CLEAR_STORE'  
+
+export function handleStore(user) {
+  return (dispatch) => {
+    dispatch(showLoading())
+    dispatch(handleLogin(user))
+    dispatch(handleGetQustions())
+    dispatch(handleGetUsers())
+    dispatch(hideLoading())
+  }
+}
+
+function ClearStore() {
+  return {type : CLEAR_STORE, clrear:true};
+}
+export function handleClearStore(){
+  return (dispatch) => {
+        dispatch(ClearStore())
+  }
+}
