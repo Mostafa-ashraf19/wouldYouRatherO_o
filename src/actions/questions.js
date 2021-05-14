8d59402482389b1fe8a9f5eac7d9f1db2985534e
Mostafa-ashraf19 <mostaphaashraf1996@gmail.com>
2021-05-14 15:16:08 +0200

    supports new functionality

--- src/actions/questions.js
+++ src/actions/questions.js
@@ -1,22 +1,45 @@
-import {_getQuestions} from '../utils/_DATA'
+import {_getQuestions, _saveQuestion} from '../utils/_DATA'
+import { showLoading, hideLoading } from 'react-redux-loading-bar'
 
-export const GET_QUESTIONS = 'GET_QUESTIONS';
+export const GET_QUESTIONS = 'GET_QUESTIONS'
+export const ADD_QUESTION = 'ADD_QUESTION'
 
 function getQuestions(questions) {
   return {
-    type: GET_QUESTIONS, 
-    questions
-    };
+    type: GET_QUESTIONS, questions
+  }
 }
 
-export function handleGetQustions() {
-    return (dispatch) => {
-        _getQuestions().then((questions) => {
-            // console.log('questions is ', questions)
-            // console.log('dispatch is', dispatch)
-            dispatch(getQuestions(questions))
-        }).catch((e) => {
-            alert('Error happen', e);
+function addQuestion(question) {
+  return {
+    type: ADD_QUESTION, question
+  }
+}
+
+export function handleAddQuestion(optionOneText, optionTwoText) {
+  return (dispatch,getState) => {
+    const {authenticate} = getState()
+    dispatch(showLoading())
+    _saveQuestion({optionOneText, optionTwoText, author:authenticate}).then((question) => {
+          dispatch(addQuestion(question));
+        }).then(()=>{
+          dispatch(hideLoading())
         })
-    }
+        .catch((e) => {
+          alert('An error occurred. Try agin. ', e);
+        })
+  }
+}
+
+
+export function handleGetQustions() {
+  return (dispatch) => {
+    _getQuestions()
+        .then((questions) => {
+          dispatch(getQuestions(questions));
+        })
+        .catch((e) => {
+          alert('An error occurred. Try agin. ', e);
+        })
+  }
 }
