7995a4c13d73ed6578867db0f9152887e5a3a5d7
Mostafa-ashraf19 <mostaphaashraf1996@gmail.com>
2021-05-16 06:43:43 +0200

    Question form component

---
+++ src/components/QuestionForm.js
@@ -0,0 +1,175 @@
+import React, {Component, Fragment} from 'react'
+import {connect} from 'react-redux'
+import {IconContext} from "react-icons"
+import {GiVote} from 'react-icons/gi'
+
+import {handleSaveAnswer} from '../actions/questions'
+class QuestionForm extends Component {
+    state = {
+        answer : ''
+    }
+    handleChange = e => {
+        const {value}  = e.target
+        this.setState({'answer':value})
+    }
+    handleSubmit = e => {
+        e.preventDefault()
+        const {answer} = this.state
+        const {dispatch} = this.props
+        const {qid} = this.props
+        dispatch(handleSaveAnswer({qid,answer}))
+    }
+    render( ) {
+
+        const {questionType,question,avatarURL,name,authedUserAnswer} = this.props
+
+        return (
+            <Fragment>
+
+                {
+                // true for unAnswered Question.
+                questionType === true && 
+                
+                <div className='Question-container'>
+                    <h3 className='h1-style'>Asked by <span>{name}</span></h3>  
+                    <div className='div-container'>
+                            
+                            <div  className='div-left'>
+                                 <img className='Question-avatar' src={avatarURL} alt={name}/>    
+                            </div>
+
+                            <div className='div-left'>
+                                <h1>Would You Rather ... </h1>
+                                <form onSubmit={this.handleSubmit}>
+                                    
+                                    <p>
+
+                                        <input 
+                                        type='radio' 
+                                        value='optionOne'
+                                        name='answer'
+                                        onChange={this.handleChange}
+                                        />
+                                        <b>{question.optionOne.text}</b>
+
+                                    </p>
+                                    <p>
+
+                                        <input 
+                                        type='radio' 
+                                        value='optionTwo'
+                                        name='answer'
+                                        onChange={this.handleChange}
+                                        />
+                                        <b>{question.optionTwo.text}</b>
+
+                                    </p>
+
+                                    <input type='submit' className='Question-btn'  values='Submit' />
+                                </form>
+                            </div>
+                    </div>            
+                        
+
+                </div>
+
+                }
+
+                { questionType === false && 
+                    //  Answered Question.  
+                    
+                    <div className='Question-container'>
+
+                        <h3 className='h1-style'>Asked by <span>{name}</span></h3>  
+
+                        <div className='div-container'>
+                            
+                            <div  className='div-left'>
+                                 <img className='Question-avatar' src={avatarURL} alt={name}/>    
+                            </div>
+
+                            <div className='div-left'>
+                                
+                                <h1>Results:</h1>
+                                
+                                {/* First Answer box*/}
+                                <div className='Question-box'>
+
+                                { authedUserAnswer === "optionOne" && 
+                                    <div className='div-icon'>
+
+
+                                    <IconContext.Provider value={{ size: '40px'}}>
+                                            <GiVote   />    
+                                        </IconContext.Provider>
+
+                                    </div>   
+                                } 
+                                     
+                                    <h4 className='center'>Would you rather {question.optionOne.text}?</h4>
+
+                                    <div className='center'>
+                                        <progress  value={question.optionOne.votes.length} max={question.optionOne.votes.length+
+                                        question.optionTwo.votes.length}
+                                        ></progress>
+                                    </div>
+
+                                    <p className='center'><span>{question.optionOne.votes.length}</span> out of <span>
+                                        {question.optionTwo.votes.length}</span> votes</p>
+                                </div>
+
+                                {/* Second Answer box*/}
+                                <div className='Question-box'>
+
+                                { authedUserAnswer === "optionTwo" && 
+                                    <div className='div-icon'>
+
+
+                                    <IconContext.Provider value={{ size: '40px'}}>
+                                            <GiVote   />    
+                                        </IconContext.Provider>
+
+                                    </div>   
+                                }
+
+                                    <h4 className='center'>Would you rather {question.optionTwo.text}?</h4>
+                                    
+                                    <div className='center'>
+                                    <progress  value={question.optionTwo.votes.length} max={question.optionOne.votes.length+
+                                        question.optionTwo.votes.length}
+                                        ></progress>
+                                    </div>
+
+                                    <p className='center'><span>{question.optionOne.votes.length}</span> out of <span>
+                                        {question.optionTwo.votes.length}</span> votes</p>
+                                </div>    
+                                
+                            </div>  
+
+                        </div>  
+
+                    </div>
+                    
+                }
+
+            </Fragment>
+        )
+
+    } 
+} 
+
+function mapstateToProps({users,questions},ownProps) {
+    const {questionType} = ownProps.location.state // true for unAnswered Question.
+    const qid = ownProps.match.params.id
+    const question = questions[qid]
+    const {avatarURL,name,answers} = users[questions[qid].author]
+    const authedUserAnswer = answers[qid]
+
+    return {
+        qid,questionType,question,avatarURL,name,authedUserAnswer
+    }
+}
+
+const CQuestionForm = connect(mapstateToProps)(QuestionForm)
+
+export default  CQuestionForm
