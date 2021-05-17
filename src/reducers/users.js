457199d1604d1a2b51281f24a5dce7e681688639
Mostafa-ashraf19 <mostaphaashraf1996@gmail.com>
2021-05-17 07:15:17 +0200

    support reducers for new actions

--- src/reducers/users.js
+++ src/reducers/users.js
@@ -1,4 +1,4 @@
-import {GET_USERS} from '../actions/users'
+import {GET_USERS,UPDATE_USER_QUESTIONS,UPDATE_USER_ANSWERS} from '../actions/users'
 
 
 export default function users(state = {}, action){
@@ -9,6 +9,26 @@ export default function users(state = {}, action){
                 ...state,
                 ...action.users
             }
+        case UPDATE_USER_QUESTIONS : 
+            
+            return {
+                ...state,
+                [action.meta.userId] : {
+                    ...state[action.meta.userId],
+                    questions : state[action.meta.userId].questions.concat([action.meta.questionId])
+                }
+            }  
+        case UPDATE_USER_ANSWERS : 
+            return {
+                ...state, 
+                [action.meta.uId] : {
+                    ...state[action.meta.uId],
+                    answers : {
+                        ...state[action.meta.uId].answers, 
+                        [action.meta.qId] : action.meta.answer
+                    }
+                }
+            }          
         default:
             return state
     }
